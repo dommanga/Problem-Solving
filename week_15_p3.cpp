fa74bc89416d217a9a412631314a7d2dced35d41
Jimi <leejm21@postech.ac.kr>
2024-06-07 17:17:24 +0900

    week15 problem C solved.

---
+++ week_15_p3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+
+    long long int X, M;
+
+    cin >> X >> M;
+
+    cout << X * (M + 1);
+}
